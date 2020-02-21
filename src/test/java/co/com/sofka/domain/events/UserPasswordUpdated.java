5859e570ae2c3365fe1fc7f9d050855c9187e48b
Raul A. Alzate <raul.alzate@sofka.com.co>
2020-02-21 06:19:38 -0500

    se realiza ajustes en el settng

--- src/test/java/co/com/sofka/domain/events/UserPasswordUpdated.java
+++ src/test/java/co/com/sofka/domain/events/UserPasswordUpdated.java
@@ -6,7 +6,7 @@ import co.com.sofka.domain.values.UserPassword;
 public  class UserPasswordUpdated extends DomainEvent {
     public final UserPassword userPassword;
 
-    protected UserPasswordUpdated(UserPassword userPassword) {
+    public UserPasswordUpdated(UserPassword userPassword) {
         super("user_password_updated");
         this.userPassword = userPassword;
     }
