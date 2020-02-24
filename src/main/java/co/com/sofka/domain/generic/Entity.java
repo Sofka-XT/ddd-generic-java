9ad3465659cfc64821d0a95bc75e89e76aaf756f
JPMS <juanpa96@live.com.mx>
2020-02-24 08:26:56 -0500

    Changes in AggregateRoot for less coupling

--- src/main/java/co/com/sofka/domain/generic/Entity.java
+++ src/main/java/co/com/sofka/domain/generic/Entity.java
@@ -1,5 +1,6 @@
 package co.com.sofka.domain.generic;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public abstract class Entity<I> {
@@ -10,5 +11,18 @@ public abstract class Entity<I> {
 
     public UUID generateIdentity(){
         return UUID.randomUUID();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity<?> entity = (Entity<?>) o;
+        return Objects.equals(entityId, entity.entityId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(entityId);
     }
 }
