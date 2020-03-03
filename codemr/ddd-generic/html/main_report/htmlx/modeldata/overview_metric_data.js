ce43e6f802c66548ed9850817ff347f111033531
Raul A. Alzate <raul.alzate@sofka.com.co>
2020-03-03 08:29:43 -0500

    se implementa la clase soporte para manejar la serializacion y deserializacion de los eventos y almace de eventos

--- codemr/ddd-generic/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"ddd-generic", "Number of Highly Problematic Classes":0, "Number of Entities":29, "Number of Problematic Classes":0, "Class Lines of Code":271, "Number of External Packages":8, "Number of Packages":7, "Number of External Entities":35}
--- codemr/ddd-generic/html/workingsets.js
+++
@@ -1,3 +0,0 @@
-var EQ_workingSetList = [
-{name: 'ddd-generic', path:'ddd-generic'}
-];
