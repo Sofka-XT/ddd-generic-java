5734435935977811a27d780daf102af86758dc88
Raul A. Alzate <raul.alzate@sofka.com.co>
2020-05-11 18:59:23 -0500

    quite a few changes of improvements and fixed.

--- co.com.sofka.application/src/main/java/module-info.java
+++ co.com.sofka.application/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module co.com.sofka.application {
-    exports co.com.sofka.application;
-    requires io.github.classgraph;
     requires java.logging;
+    requires io.github.classgraph;
 
     requires co.com.sofka.infraestructure;
     requires co.com.sofka.business;
     requires co.com.sofka.domain;
+
+    exports co.com.sofka.application;
+
 }
